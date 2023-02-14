c391acd9c9698ae2b3f1c9ae79c9e55c940ba8fb
Giovanni Marcatto <gzmarcatto@gmail.com>
2023-02-14 14:49:01 -0300

    fix: ajuste na query de insert

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -4,10 +4,13 @@ export async function postGames(req, res) {
   try {
     const { name, image, stockTotal, pricePerDay } = req.body;
 
-    await db.query(
-      'INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);'
-    )[(name, image, stockTotal, pricePerDay)];
+    const existsGame = await db.query(`SELECT * FROM games WHERE name=($1)`, [name]);
+    if (existsGame.rowCount > 0) return res.sendStatus(409);
 
+    await db.query(
+      'INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);',
+      [name, image, stockTotal, pricePerDay]
+    );
     return res.sendStatus(201);
   } catch (error) {
     console.log(error);
